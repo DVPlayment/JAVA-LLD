4a04a49c10a1ace0187b0bf69c70d0d0eb58962c
dvuchiha <devansh.verma@telusinternational.com>
2024-11-26 12:55:00 +0530

    Added hashmap implementation from scratch

---
+++ src/main/java/org/example/HashmapImplementation/LinkedListCustom.java
@@ -0,0 +1,12 @@
+package org.example.HashmapImplementation;
+
+public class LinkedListCustom {
+    protected int key;
+    protected String value;
+    protected LinkedListCustom next;
+    public LinkedListCustom(int key, String value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+}
---
+++ src/main/java/org/example/HashmapImplementation/Main.java
@@ -0,0 +1,24 @@
+package org.example.HashmapImplementation;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMapCustom map = new HashMapCustom(5, 0.75);
+        map.put(1,"hello");
+        map.put(2,"friend");
+        map.put(10,"Elliot");
+        map.put(5,"hello");
+        System.out.println(map.capacity());
+        map.put(6,"friend");
+        map.put(7,"Elliot");
+
+        System.out.println(map.get(10));
+        System.out.println(map.get(2));
+        System.out.println(map.get(1));
+
+        System.out.println(map.get(10000));
+
+        System.out.println(map.capacity());
+    }
+}
